f92c5e71a150a3426251d00c5740ccbbebdef41b
alexge50 <alexge50.x@gmail.com>
2018-12-25 21:33:34 +0200

    made correct test

--- matrix/test/main.cu
+++ matrix/test/main.cu
@@ -1,43 +1,24 @@
 #include <iostream>
-#include "../include/cuda-indexing.h"
 #include <Matrix.h>
-
-__global__
-void multiply_matrix(int n, Matrix a, Matrix b, Matrix *c)
-{
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    int line = index / n;
-    int column = index % n;
-
-    for(int k = 0; k < n; k++)
-        (*c)[line][column] += a[line][k] * b[k][column];
-
-    return ;
-}
+#include <Multiplication.h>
 
 int main()
 {
     constexpr int n = 100;
-    Matrix a(n), b(n), *c;
-    cudaMallocManaged(&c, sizeof(Matrix));
-
-    new(c)Matrix(n);
+    Matrix a(n), b(n);
 
     for(int i = 0; i < n; i++)
         for(int j = 0; j < n; j++)
             a[i][j] = b[i][j] = (i == j);
 
-    multiply_matrix<<<(n * n + 256 - 1) / 256, 256>>>(n, a, b, c);
-    cudaDeviceSynchronize();
+    Matrix c = multiply_matrix(a, b);
 
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
-            std::cout << (*c)[i][j] << " ";
+            std::cout << c[i][j] << " ";
         std::cout << "\n";
     }
 
-    cudaFree(c);
     return 0;
 }
